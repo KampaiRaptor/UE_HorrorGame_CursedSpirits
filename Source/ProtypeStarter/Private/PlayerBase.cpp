c19828b62e119c4cdafb720474c918c9da4882e7
Fansi129 <120315901+Fansi129@users.noreply.github.com>
2024-03-12 23:02:10 +0100

    Player movement

--- Source/ProtypeStarter/Private/PlayerBase.cpp
+++ Source/ProtypeStarter/Private/PlayerBase.cpp
@@ -8,27 +8,23 @@ APlayerBase::APlayerBase()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
 // Called when the game starts or when spawned
 void APlayerBase::BeginPlay()
 {
 	Super::BeginPlay();
-	
 }
 
 // Called every frame
 void APlayerBase::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-
 }
 
 // Called to bind functionality to input
 void APlayerBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
 }
 
